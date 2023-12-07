31df4579666b6a18d474f0d364048b5057c0dfe0
Alvaro <alvaromagu2002@gmail.com>
2023-12-07 19:01:46 +0100

    day 05: reduce complexity

--- src/05/index.ts
+++ src/05/index.ts
@@ -1,18 +1,14 @@
 export function cyberReindeer (road: string, time: number): string[] {
+  const obj: Record<any, string> = { '*': '*S', '|': '*S' }
   const stack: string[] = []
   let index = 0
   for (let i = 1; i <= time; i++) {
     stack.push(road)
     if (i === 5) road = road.replaceAll('|', '*')
-    if (road[index + 1] === '.' || road[index + 1] === '*') {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      if (stack.at(-2) != null && (stack.at(-2)![index] === '*' || stack.at(-2)![index] === '|')) {
-        road = road.substring(0, index) + '*S' + road.substring(index + 2, road.length)
-      } else {
-        road = road.substring(0, index) + '.S' + road.substring(index + 2, road.length)
-      }
-      index++
-    }
+    if (road[index + 1] !== '.' && road[index + 1] !== '*') continue
+    const step = obj[stack.at(-2)?.[index] ?? ''] ?? '.S'
+    road = road.slice(0, index) + step + road.slice(index + 2)
+    index++
   }
   return stack
 }
