8a5df79be60d7bbe929f4289fa9368ad59ba0534
Alvaro <alvaromagu2002@gmail.com>
2023-12-07 21:32:34 +0100

    fix day 6 (not passing tests)

--- src/06/index.ts
+++ src/06/index.ts
@@ -1,7 +1,5 @@
 export function maxDistance (movements: string): number {
   const left = movements.match(/</g)?.length ?? 0
   const right = movements.match(/>/g)?.length ?? 0
-  const diff = right - left
-  const any = movements.length - left + right
-  return Math.abs(diff) + Math.abs(any)
+  return Math.abs(right - left) + movements.length - (left + right)
 }
