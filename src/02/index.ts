6031518885e48af12a65faebb75004ed2e8bb38f
Alvaro <alvaromagu2002@gmail.com>
2023-12-08 15:55:38 +0100

    reduce day 2 complexity

--- src/02/index.ts
+++ src/02/index.ts
@@ -1,5 +1,9 @@
 export function manufacture (gifts: string[], materials: string): string[] {
+  materials = [...new Set(materials.split(''))].join('')
   return gifts.filter(gift => {
-    return gift.split('').every(char => materials.includes(char))
+    const uc = gift.match(/(.)(?!\1)/g)?.join('') ?? ''
+    console.log(uc)
+    const regex = new RegExp(`[${uc}]`, 'g')
+    return materials.match(regex)?.length === uc.length
   })
 }
